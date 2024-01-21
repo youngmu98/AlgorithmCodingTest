bce12a154c98fad1548798da8aab3467ecefa83c
dbdudann <dbdudann@gmail.com>
2024-01-21 09:38:04 +0900

    음료수 얼려 먹기 p.149

---
+++ src/this_is_coding_test/DFSandBFS/ColdBeverage.java
@@ -0,0 +1,54 @@
+package this_is_coding_test.DFSandBFS;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class ColdBeverage {
+    static int n, m, result;
+    static int[][] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = line.charAt(j)-'0';
+            }
+        }
+
+        result = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (dfs(i,j)){
+                    result = result + 1;
+                }
+            }
+        }
+
+        System.out.println(result);
+
+    }
+
+    static boolean dfs(int i, int j) {
+        if (i < 0 || i >= n || j < 0 || j >= m) {
+            return false;
+        }
+        if (arr[i][j] == 0) {
+            arr[i][j] = 1;
+            dfs(i - 1, j);
+            dfs(i + 1, j);
+            dfs(i, j - 1);
+            dfs(i, j + 1);
+            return true;
+        }
+        return false;
+    }
+}
