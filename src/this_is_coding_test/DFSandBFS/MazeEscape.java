61bd612977d1772080370b08951eb077596935d5
dbdudann <dbdudann@gmail.com>
2024-01-29 10:06:57 +0900

    미로 탈출 p.152

---
+++ src/this_is_coding_test/DFSandBFS/MazeEscape.java
@@ -0,0 +1,75 @@
+package this_is_coding_test.DFSandBFS;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+// 미로 탈출 Page.152
+// 풀이 실패 - 참고 github.ndb796
+class Node {
+    private int x;
+    private int y;
+
+    public Node(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
+
+public class MazeEscape {
+    static int n, m;
+    static int[][] arr;
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        arr = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = str.charAt(j) - '0';
+            }
+        }
+
+        System.out.println(bfs(0, 0));
+
+    }
+
+    static int bfs(int x, int y) {
+        Queue<Node> q = new LinkedList<>();
+        q.offer(new Node(x, y));
+        while (!q.isEmpty()) {
+            Node node = q.poll();
+            x = node.getX();
+            y = node.getY();
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+                if (arr[nx][ny] == 0) continue;
+                if (arr[nx][ny] == 1) {
+                    arr[nx][ny] = arr[x][y] + 1;
+                    q.offer(new Node(nx, ny));
+                }
+            }
+        }
+        return arr[n - 1][m - 1];
+    }
+}
